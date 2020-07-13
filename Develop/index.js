8ef522172705b5900cd518c8d1302868626ee70b
Seyoung Yoon <stacey.exp@gmail.com>
2020-07-12 22:25:57 -0700

    added questions and write file

--- Develop/index.js
+++ Develop/index.js
@@ -1,64 +1,96 @@
 const questions = [
-    {
+        {
               type: "input",
-              message: "What is your user name?",
-              name: "username"
-            },
-            {
-              type: "checkbox",
-              message: "What languages do you know?",
-              choices: ["HTML","Javascript","C++"],
-              name: "password"
+              message: "What is the title of the project?",
+              name: "title"
             },
             {
               type: "input",
-              message: "What's your preferred method of communication?",
-              name: "confirm"
-            }
-
+              message: "Give a description of the project.",
+              name: "projDes"
+            },
+            {
+              type: "input",
+              message: "Please include any Installations required.",
+              name: "installation"
+            },
+            {
+              type: "input",
+              message: "Please provide any Usage information.",
+              name: "usage"
+            },
+            {
+              type: "input",
+              message: "What licenses are used?",
+              name: "license"
+            },
+            {
+              type: "input",
+              message: "Give information about contributors",
+              name: "contribute"
+            },
+            {
+              type: "input",
+              message: "What questions were asked?",
+              name: "question"
+            },
+            {
+              type: "input",
+              message: "What tests were used?",
+              name: "test"
+            },
+            {
+              type: "input",
+              message: "Please provide your GitHub username",
+              name: "github"
+            },
 ];
 var inquirer = require("inquirer");
 var prompt = inquirer.createPromptModule();
 
 prompt(questions).then(function(response){
-    console.log(response)
+  const title = response.title;
+  const des = response.projDes;
+  const install = response.install;
+  const usage = response.usage;
+  const license = response.license;
+  const contribute = response.contribute;
+  const test = response.test;
+  const question = response.question;
+  const github = response.github;
+
+  const readMe = 
+('# ${title}' + title + '\n' +
+
+'# Table of Contents'+ '\n' +
+// ${Table of Contents},
+'# Installations \n'+
+install + '\n'+
+'# Usage \n'+
+usage + '\n'+
+'# Licenses' + '\n'+
+license + '\n'+
+"# Contributing" + '\n'+
+contribute + '\n'+
+'# Tests' + '\n'+
+test + '\n'+
+'# Questions' + '\n'+ question + '\n' +
+'# GitHub Information'+ '\n'+ github)
+// ${GitHub Email}';
+console.log(readMe)
+
+var fs = require("fs");
+
+fs.writeFile("README.mb",readMe,function(err){
+if (err){
+    console.log("oh")
+}
 })
-// inquirer
-//   .prompt([
-//     {
-//       type: "input",
-//       message: "What is your user name?",
-//       name: "username"
-//     },
-//     {
-//       type: "checkbox",
-//       message: "What languages do you know?",
-//       choices: ["HTML","Javascript","C++"],
-//       name: "password"
-//     },
-//     {
-//       type: "input",
-//       message: "What's your preferred method of communication?",
-//       name: "confirm"
-//     }
-//   ])
-//   .then(function(response) {
-//       questions.push(response.name)
-//   })
-// console.log(questions)
+})
 
-// function writeToFile(fileName, data) {
-//     var fs = require("fs");
 
-//     fs.appendFile("message.txt",JSON.stringify(response),function(err){
-//     if (err){
-//         console.log("oh")
-//     }
-//   })
-// }
-
-// function init() {
-
-// }
+function init() {
+ 
+}
 
 // init();
--- Develop/read.md
+++ Develop/read.md
@@ -1,1 +1,40 @@
 # ReadMeGenerator
+The README will be populated with the following:
+
+At least one badge
+Project title
+Description
+Table of Contents
+Installation
+Usage
+License
+Contributing
+Tests
+Questions
+
+User GitHub profile picture
+User GitHub email
+# Minimum Requirements
+
+
+Functional, deployed application.
+
+
+GitHub repository with a unique name and a README describing project.
+
+
+The generated README includes the following sections:
+
+Title
+Description
+Table of Contents
+Installation
+Usage
+License
+Contributing
+Tests
+Questions
+
+
+
+The generated README includes 1 badge that's specific to the repository.
